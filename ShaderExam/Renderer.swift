a381b32acfae2cb14b64885fda50a97b18f889b8
Zsolt Ero <zsolt.ero@gmail.com>
2018-01-31 22:56:38 +0100

    Live coding shaders (#1)
    Merging live-coding enhancements from @hyperknot:
      - Disabled sandbox
      - Removed entitlements
      - Enabled live-coding by eagerly reloading shader source every 10 frames
      - Linted with swiftformat

--- ShaderExam/Renderer.swift
+++ ShaderExam/Renderer.swift
@@ -7,73 +7,77 @@ struct Uniforms {
     let projectionMatrix: float4x4
 }
 
-class Renderer : NSObject, MTKViewDelegate {
-    
+class Renderer: NSObject, MTKViewDelegate {
+
+    let SHADER_RELOAD_FRAME = 10
+
     let view: MTKView
     let device: MTLDevice
     let commandQueue: MTLCommandQueue
-    let mainRenderPipeline: MTLRenderPipelineState?
-    let postRenderPipeline: MTLRenderPipelineState?
+    var mainRenderPipeline: MTLRenderPipelineState?
+    var postRenderPipeline: MTLRenderPipelineState?
     let depthStencilState: MTLDepthStencilState
     var mesh: MTKMesh?
     var texture: MTLTexture?
     var targets: [MTLTexture] = []
-    
+
+    let projectDir: String
+    var shaderString = ""
+    let vertexDescriptor: MTLVertexDescriptor
+    let quadVertexDescriptor: MTLVertexDescriptor
+    var frameCount = 0
+
     init(view: MTKView, device: MTLDevice) {
+
         self.view = view
         self.device = device
-        self.commandQueue = device.makeCommandQueue()!
-        
+        commandQueue = device.makeCommandQueue()!
+
         guard let modelURL = Bundle.main.url(forResource: "pikachu", withExtension: "obj", subdirectory: "pikachu") else {
             fatalError("Could not find model file in main bundle")
         }
-        
+
+        guard let projectDir_ = ProcessInfo.processInfo.environment["PROJECT_DIR"] else {
+            fatalError("PROJECT_DIR environment variable not passed at runtime, please make sure Scheme contains it")
+        }
+        projectDir = projectDir_
+
         let mdlVertexDescriptor = MDLVertexDescriptor()
         mdlVertexDescriptor.attributes[0] = MDLVertexAttribute(name: MDLVertexAttributePosition, format: MDLVertexFormat.float3, offset: 0, bufferIndex: 0)
         mdlVertexDescriptor.attributes[1] = MDLVertexAttribute(name: MDLVertexAttributeNormal, format: MDLVertexFormat.float3, offset: 12, bufferIndex: 0)
         mdlVertexDescriptor.attributes[2] = MDLVertexAttribute(name: MDLVertexAttributeTextureCoordinate, format: MDLVertexFormat.float2, offset: 24, bufferIndex: 0)
         mdlVertexDescriptor.layouts[0] = MDLVertexBufferLayout(stride: 32)
-        
+
         let bufferAllocator = MTKMeshBufferAllocator(device: device)
         let mdlAsset = MDLAsset(url: modelURL, vertexDescriptor: mdlVertexDescriptor, bufferAllocator: bufferAllocator)
-        
+
         if let firstMDLMesh = mdlAsset.childObjects(of: MDLMesh.self).first as? MDLMesh {
             mesh = try? MTKMesh(mesh: firstMDLMesh, device: device)
         }
-        
+
         guard let textureURL = Bundle.main.url(forResource: "pikachu", withExtension: "png", subdirectory: "pikachu") else {
             fatalError("Could not find model texture file in main bundle")
         }
-        
+
         let textureLoader = MTKTextureLoader(device: device)
-        
-        let textureOptions = [ MTKTextureLoader.Option.SRGB : false,
-                               MTKTextureLoader.Option.origin : MTKTextureLoader.Origin.flippedVertically ] as [MTKTextureLoader.Option : Any]
+
+        let textureOptions = [
+            MTKTextureLoader.Option.SRGB: false,
+            MTKTextureLoader.Option.origin: MTKTextureLoader.Origin.flippedVertically,
+        ] as [MTKTextureLoader.Option: Any]
         texture = try? textureLoader.newTexture(URL: textureURL, options: textureOptions)
-        
-        guard let library = device.makeDefaultLibrary() else {
-            fatalError("Could not load default Metal library from main bundle")
-        }
-        
-        guard let vertexDescriptor = MTKMetalVertexDescriptorFromModelIO(mdlVertexDescriptor) else {
+
+        guard let vertexDescriptor_ = MTKMetalVertexDescriptorFromModelIO(mdlVertexDescriptor) else {
             fatalError("Could not form MTL vertex descriptor from MDL vertex descriptor")
         }
-        
-        let mainRenderPipelineDescriptor = MTLRenderPipelineDescriptor()
-        mainRenderPipelineDescriptor.colorAttachments[0].pixelFormat = .rgba8Unorm
-        mainRenderPipelineDescriptor.depthAttachmentPixelFormat = .depth32Float
-        mainRenderPipelineDescriptor.vertexDescriptor = vertexDescriptor
-        mainRenderPipelineDescriptor.vertexFunction = library.makeFunction(name: "vertex_main")
-        mainRenderPipelineDescriptor.fragmentFunction = library.makeFunction(name: "fragment_main")
-        
-        mainRenderPipeline = try? device.makeRenderPipelineState(descriptor: mainRenderPipelineDescriptor)
-        
+        vertexDescriptor = vertexDescriptor_
+
         let depthStencilDescriptor = MTLDepthStencilDescriptor()
         depthStencilDescriptor.depthCompareFunction = .less
         depthStencilDescriptor.isDepthWriteEnabled = true
         depthStencilState = device.makeDepthStencilState(descriptor: depthStencilDescriptor)!
-        
-        let quadVertexDescriptor = MTLVertexDescriptor()
+
+        quadVertexDescriptor = MTLVertexDescriptor()
         quadVertexDescriptor.attributes[0].format = .float2
         quadVertexDescriptor.attributes[0].offset = 0
         quadVertexDescriptor.attributes[0].bufferIndex = 0
@@ -81,16 +85,11 @@ class Renderer : NSObject, MTKViewDelegate {
         quadVertexDescriptor.attributes[1].offset = 8
         quadVertexDescriptor.attributes[1].bufferIndex = 0
         quadVertexDescriptor.layouts[0].stride = 16
-        
-        let postRenderPipelineDescriptor = MTLRenderPipelineDescriptor()
-        postRenderPipelineDescriptor.colorAttachments[0].pixelFormat = view.colorPixelFormat
-        postRenderPipelineDescriptor.vertexDescriptor = quadVertexDescriptor
-        postRenderPipelineDescriptor.vertexFunction = library.makeFunction(name: "vertex_post")
-        postRenderPipelineDescriptor.fragmentFunction = library.makeFunction(name: "fragment_post")
-        
-        postRenderPipeline = try? device.makeRenderPipelineState(descriptor: postRenderPipelineDescriptor)
+
+        super.init()
+        _ = loadShaders()
     }
-    
+
     func makeOffscreenTargets(_ size: CGSize) {
         let descriptor = MTLTextureDescriptor.texture2DDescriptor(pixelFormat: .invalid,
                                                                   width: Int(size.width),
@@ -103,20 +102,25 @@ class Renderer : NSObject, MTKViewDelegate {
 
         descriptor.pixelFormat = .depth32Float
         descriptor.usage = .renderTarget
-        descriptor.storageMode = .`private`
+        descriptor.storageMode = .private
         let depthTarget = device.makeTexture(descriptor: descriptor)!
-        
+
         targets = [colorTarget, depthTarget]
     }
-    
+
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
         makeOffscreenTargets(size)
     }
-    
+
     func draw(in view: MTKView) {
+        frameCount += 1
+        if frameCount % SHADER_RELOAD_FRAME == 0 {
+            _ = loadShaders()
+        }
+
         guard let commandBuffer = commandQueue.makeCommandBuffer() else { return }
         guard let mesh = mesh else { return }
-        
+
         let mainPassDescriptor = MTLRenderPassDescriptor()
         mainPassDescriptor.colorAttachments[0].texture = targets[0]
         mainPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 0.95, green: 0.95, blue: 0.95, alpha: 1.0)
@@ -126,11 +130,11 @@ class Renderer : NSObject, MTKViewDelegate {
         mainPassDescriptor.depthAttachment.clearDepth = 1.0
         mainPassDescriptor.depthAttachment.loadAction = .clear
         mainPassDescriptor.depthAttachment.storeAction = .dontCare
-        
+
         guard let mainCommandEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: mainPassDescriptor) else { return }
 
         mainCommandEncoder.setDepthStencilState(depthStencilState)
-        
+
         if let renderPipeline = mainRenderPipeline {
             mainCommandEncoder.setRenderPipelineState(renderPipeline)
         }
@@ -138,19 +142,19 @@ class Renderer : NSObject, MTKViewDelegate {
         for (index, vertexBuffer) in mesh.vertexBuffers.enumerated() {
             mainCommandEncoder.setVertexBuffer(vertexBuffer.buffer, offset: vertexBuffer.offset, index: index)
         }
-        
+
         if let texture = texture {
             mainCommandEncoder.setFragmentTexture(texture, index: 0)
         }
-        
+
         let modelTransform = float4x4(translationBy: float3(0, -1.1, 0)) * float4x4(scaleBy: Float(1 / 4.5))
         let cameraTransform = float4x4(translationBy: float3(0, 0, -4))
         let aspectRatio = Float(view.drawableSize.width / view.drawableSize.height)
         let projectionMatrix = float4x4(perspectiveProjectionFov: .pi / 6, aspectRatio: aspectRatio, nearZ: 0.1, farZ: 100)
         var uniforms = Uniforms(modelViewMatrix: cameraTransform * modelTransform, projectionMatrix: projectionMatrix)
-        
+
         mainCommandEncoder.setVertexBytes(&uniforms, length: MemoryLayout.size(ofValue: uniforms), index: 1)
-        
+
         if let submesh = mesh.submeshes.first {
             let indexBuffer = submesh.indexBuffer
             mainCommandEncoder.drawIndexedPrimitives(type: submesh.primitiveType,
@@ -159,34 +163,77 @@ class Renderer : NSObject, MTKViewDelegate {
                                                      indexBuffer: indexBuffer.buffer,
                                                      indexBufferOffset: indexBuffer.offset)
         }
-        
+
         mainCommandEncoder.endEncoding()
-        
+
         guard let postRenderPassDescriptor = view.currentRenderPassDescriptor else { return }
         guard let postCommandEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: postRenderPassDescriptor) else { return }
-        
+
         if let renderPipeline = postRenderPipeline {
             postCommandEncoder.setRenderPipelineState(renderPipeline)
         }
-        
+
         postCommandEncoder.setFragmentTexture(targets[0], index: 0)
-        
+
         let vertexData: [Float] = [
             -1, -1, 0, 1,
-            -1,  1, 0, 0,
-             1, -1, 1, 1,
-             1,  1, 1, 0,
+            -1, 1, 0, 0,
+            1, -1, 1, 1,
+            1, 1, 1, 0,
         ]
-        
+
         postCommandEncoder.setVertexBytes(vertexData, length: 16 * 4, index: 0)
-        
+
         postCommandEncoder.drawPrimitives(type: .triangleStrip, vertexStart: 0, vertexCount: 4)
-        
+
         postCommandEncoder.endEncoding()
-        
+
         guard let drawable = view.currentDrawable else { return }
         commandBuffer.present(drawable)
-        
+
         commandBuffer.commit()
+    }
+
+    func loadShaders() -> Bool {
+        let shadersPath = projectDir + "/ShaderExam/Shaders.metal"
+        let quadShadersPath = projectDir + "/ShaderExam/QuadShaders.metal"
+
+        guard let shaderStringNew = try?
+            String(contentsOfFile: shadersPath) + "\n" +
+            String(contentsOfFile: quadShadersPath) else {
+            print("Could not read Metal shader file")
+            return false
+        }
+
+        if shaderString == shaderStringNew {
+            return true
+        } else {
+            shaderString = shaderStringNew
+        }
+
+        guard let library = try? device.makeLibrary(source: shaderString, options: nil) else {
+            print("Could not compile Metal library from shader file")
+            return false
+        }
+
+        let mainRenderPipelineDescriptor = MTLRenderPipelineDescriptor()
+        mainRenderPipelineDescriptor.colorAttachments[0].pixelFormat = .rgba8Unorm
+        mainRenderPipelineDescriptor.depthAttachmentPixelFormat = .depth32Float
+        mainRenderPipelineDescriptor.vertexDescriptor = vertexDescriptor
+        mainRenderPipelineDescriptor.vertexFunction = library.makeFunction(name: "vertex_main")!
+        mainRenderPipelineDescriptor.fragmentFunction = library.makeFunction(name: "fragment_main")!
+
+        mainRenderPipeline = try? device.makeRenderPipelineState(descriptor: mainRenderPipelineDescriptor)
+
+        let postRenderPipelineDescriptor = MTLRenderPipelineDescriptor()
+        postRenderPipelineDescriptor.colorAttachments[0].pixelFormat = view.colorPixelFormat
+        postRenderPipelineDescriptor.vertexDescriptor = quadVertexDescriptor
+        postRenderPipelineDescriptor.vertexFunction = library.makeFunction(name: "vertex_post")!
+        postRenderPipelineDescriptor.fragmentFunction = library.makeFunction(name: "fragment_post")!
+
+        postRenderPipeline = try? device.makeRenderPipelineState(descriptor: postRenderPipelineDescriptor)
+
+        print("Shaders loaded and compiled successfully")
+        return true
     }
 }
